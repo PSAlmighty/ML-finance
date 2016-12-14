907339a1bae108705b617b1d9632116d2dc3b685
hiteshgulati <hg0111@gmail.com>
2016-12-14 18:05:46 +0530

    extraction in progress

--- practice.py
+++ practice.py
@@ -2,16 +2,68 @@ from yahoo_finance import Share
 from pprint import pprint
 import pandas as pd
 import numpy as np
+import pandas_datareader as pdr
+import pandas_datareader.data as web
+from datetime import datetime
 
-symbol_file_name = "nasdaq_retail.csv"
-symbol = 'BBY'
-share = Share(symbol)
+ticker_file_name = "data/nasdaq_retail.csv"
+ticker_file = pd.read_csv(ticker_file_name)
+# print type(ticker_file)
+# print ticker_file.columns.values
+ticker_file.drop('Unnamed: 3', axis=1,inplace=True)
+# print ticker_file.columns.values
+# print ticker_file
+# print len(ticker_file)
+start = datetime(2010,1,1)
+end = datetime(2010,1,5)
+print ticker_file['Company Name'][0]
+for i in range(0,len(ticker_file)):
+    company_name = ticker_file['Company Name'][i]
+    company_ticker = ticker_file['Ticker'][i]
+    print company_name, company_ticker
+    historical_data = pd.DataFrame()
+    historical_data = web.DataReader(company_ticker,data_source='yahoo',start=start,end=end)
+    output_file_name = "data/" + company_ticker + ".csv"
+    historical_data.to_csv (output_file_name, sep=',')
 
-print share.get_change()
-print share.get_dividend_pay_date()
-historical_prices = share.get_historical('2016-12-08', '2016-12-12')
-print type(historical_prices)
-print type (historical_prices[0])
-pprint(historical_prices)
-pprint(historical_prices[0])
+# ibm = pd.DataFrame
+# ibm = web.DataReader('BBY',data_source='yahoo',start=start,end=end)
+# file_name = 'data/out.csv'
+# print ibm
+# print type(ibm)
+# print len(ibm)
+# print ibm.ix[end]
+# ibm.to_csv(file_name, sep=',')
+
+# symbols = ['IBM', 'BBY']
+# pdr.get_data_yahoo('AAPL')
+# ibm = pdr.get_data_yahoo(symbols='IBM', start =datetime(2000,1,1), end = datetime(2000,1,5))
+# print ibm
+# symbol_file_name = "nasdaq_retail.csv"
+# symbol = 'BBY'
+# share = Share(symbol)
+#
+# # print share.get_change()
+# # print share.get_dividend_pay_date()
+# historical_prices = share.get_historical('2016-12-08', '2016-12-12')
+# # print type(historical_prices)
+# # print type (historical_prices[0])
+# #pprint(historical_prices)
+# # pprint(historical_prices[0])
+# adict = {}
+# bdict = {'a2': 22, 'b2': 23, 'c2': 24}
+# # print adict
+# # print bdict
+# # print type(adict)
+# # for item in adict:
+# #     print adict[item]
+# # aSeries = pd.Series(adict)
+# # print aSeries
+# # print type(aSeries)
+# series = pd.Series (data=historical_prices[0])
+# df = pd.DataFrame (data=series)
+# print series["Date"]
+# print df
+
+
 
--- util.py
+++ util.py
@@ -6,6 +6,8 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from yahoo_finance import Share
+from pprint import pprint
 
 def symbol_to_path (symbol, base_dir = 'data'):
     return  os.path.join ( base_dir, '{}.csv'.format(symbol))
@@ -27,13 +29,13 @@ def plot_data(data, title = 'Stock Prices'):
     ax.set_ylabel("Prices")
     plt.show()
 
-def get_rolling_mean (values, window=20)
+def get_rolling_mean (values, window=20):
     return values.rolling(window).mean()
 
 def get_rolling_std (values, window=20):
     return values.rolling(window).std()
 
-def get_bollinger_band (values, window = 20, size = 2)
+def get_bollinger_band (values, window = 20, size = 2):
     upper_band = get_rolling_mean(values,window) + get_rolling_std(values,window) * 2
     lower_band = get_rolling_mean(values, window) + get_rolling_std(values, window) * 2
     return upper_band , lower_band
